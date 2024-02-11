a3376489243e12b8690f06dd0f99a6843b90691c
ydah <13041216+ydah@users.noreply.github.com>
2024-02-12 01:41:38 +0900

    first commit

---
+++ lib/irb/theme/iceberg.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "iceberg/version"
+
+module Irb
+  module Theme
+    module Iceberg
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
