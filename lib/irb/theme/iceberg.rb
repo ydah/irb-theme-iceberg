b924ca8a6d5dd0f6852a269580822bb1632e63e1
ydah <13041216+ydah@users.noreply.github.com>
2024-02-12 20:13:25 +0900

    Adjust the color of the scroll bars

--- lib/irb/theme/iceberg.rb
+++ lib/irb/theme/iceberg.rb
@@ -3,5 +3,5 @@
 Reline::Face.config(:completion_dialog) do |conf|
   conf.define(:default, foreground: "#C0C2CB", background: "#161821")
   conf.define(:enhanced, foreground: "#84A0C6", background: "#2A3158")
-  conf.define(:scrollbar, foreground: "#333853", background: "#2A3158")
+  conf.define(:scrollbar, foreground: "#333853", background: "#161821")
 end
