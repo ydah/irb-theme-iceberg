68c728090de639b7f4722d955b72d124c9364bd6
ydah <13041216+ydah@users.noreply.github.com>
2024-02-12 18:43:10 +0900

    Adjusting Color Settings

--- lib/irb/theme/iceberg.rb
+++ lib/irb/theme/iceberg.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Reline::Face.config(:completion_dialog) do |conf|
-  conf.define(:default, foreground: "#c6c8d1", background: "#88B0C1")
-  conf.define(:enhanced, foreground: "#c6c8d1", background: "#444b71")
-  conf.define(:scrollbar, foreground: "#c6c8d1", background: "#818596")
+  conf.define(:default, foreground: "#C0C2CB", background: "#161821")
+  conf.define(:enhanced, foreground: "#84A0C6", background: "#2A3158")
+  conf.define(:scrollbar, foreground: "#333853", background: "#2A3158")
 end
